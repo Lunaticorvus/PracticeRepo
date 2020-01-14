87b6f9281a46284ca77435bfd9d0b0b250405d80
Lunaticorvus <Lunaticorvus@gmail.com>
2020-01-15 00:40:14 +0900

    add serverless finch

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 export SERVICE="practicerepo"
 export STAGE="dev"
 
-npm install && sls deploy -v && cd - || exit 1
-
 BUCKET=$(aws cloudformation describe-stack-resource --stack-name "${SERVICE}-${STAGE}" --logical-resource-id "S3BucketFinch" --query "StackResourceDetail.PhysicalResourceId" --output text)
 
 cd stack && npm install && sls deploy -v && cd - || exit 1
 
 aws s3 sync ./client/dist "s3://$BUCKET" || exit 1
+
+npm install && sls deploy -v && cd - || exit 1
