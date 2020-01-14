5b134e8cce61599ea5c14141e7f61eff57789a6e
Lunaticorvus <Lunaticorvus@gmail.com>
2020-01-15 00:13:26 +0900

    add finch plugin

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+export SERVICE="practicerepo"
+export STAGE="dev"
+
+npm install && sls deploy -v && cd - || exit 1
+
+BUCKET=$(aws cloudformation describe-stack-resource --stack-name "${SERVICE}-${STAGE}" --logical-resource-id "S3BucketFinch" --query "StackResourceDetail.PhysicalResourceId" --output text)
 
 cd stack && npm install && sls deploy -v && cd - || exit 1
+
+aws s3 sync ./client/dist "s3://$BUCKET" || exit 1
