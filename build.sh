19f5a48d187e51d180cd94afdc91c9e22cf3cece
Lunaticorvus <Lunaticorvus@gmail.com>
2020-01-15 21:11:33 +0900

    type revise

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@ export SERVICE_FINCH="practicerepo-finch"
 export SERVICE_STACK="practicerepo-stack"
 export STAGE="dev"
 
-BUCKET=$(aws cloudformation describe-stack-resource --stack-name "${SERVICE}-${STAGE}" --logical-resource-id "S3BucketFinch" --query "StackResourceDetail.PhysicalResourceId" --output text)
+BUCKET=$(aws cloudformation describe-stack-resource --stack-name "${SERVICE_FINCH}-${STAGE}" --logical-resource-id "S3BucketFinch" --query "StackResourceDetail.PhysicalResourceId" --output text)
 
 cd stack && npm install && sls deploy -v && cd - || exit 1
 
-aws s3 sync ./client/dist "s3://$BUCKET" || exit 1
-
 npm install && sls deploy -v && cd - || exit 1
+
+aws s3 sync ./client/dist "s3://$BUCKET" || exit 1
