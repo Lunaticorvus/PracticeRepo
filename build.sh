14338c8d3788e450c53666b4dcabb85350843f6c
Lunaticorvus <Lunaticorvus@gmail.com>
2020-01-21 00:12:09 +0900

    fix: remove unnecessary command

--- build.sh
+++ build.sh
@@ -9,6 +9,6 @@ cd stack && npm install && sls deploy -v && cd - || exit 1
 
 npm install && sls client deploy -v --no-confirm
 
-aws s3 sync "./client/dist" "s3://$SERVICE_FINCH-$STAGE" || exit 1
+#aws s3 sync "./client/dist" "s3://$SERVICE_FINCH-$STAGE" || exit 10
 
 echo "https://practicerepo-finch-dev.s3.ap-northeast-2.amazonaws.com/index.html"
